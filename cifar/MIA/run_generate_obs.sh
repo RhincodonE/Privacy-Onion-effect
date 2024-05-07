ccbc0198d1999df0414ddad2209272632095ef4d
RhincodonE <111275412+RhincodonE@users.noreply.github.com>
2024-05-06 20:15:32 -0500

    Update run_generate_obs.sh

--- cifar/MIA/run_generate_obs.sh
+++ cifar/MIA/run_generate_obs.sh
@@ -8,18 +8,18 @@ number_shadow_model=1
 # Make a copy of the original YAML to preserve it
 cp $yaml_file $original_yaml_file
 
-#for i in $(seq 1 $number_shadow_model); do
-#    sed -i "s|in_model_save_path: ./data/models_shadow/.*|in_model_save_path: ./data/models_shadow/model_in_${i}.pth|" $yaml_file
-#    sed -i "s|out_dataset: ./data/tmp_shadow/cifar_out.*|out_dataset: ./data/tmp_shadow/cifar_out_${i}.beton|" $yaml_file
-#    sed -i "s|in_dataset: ./data/tmp_shadow/cifar_in.*|in_dataset: ./data/tmp_shadow/cifar_in_${i}.beton|" $yaml_file
-#    sed -i "s|out_dataset_index: ./data/tmp_shadow/out.*|out_dataset_index: ./data/tmp_shadow/out_${i}.npy|" $yaml_file
-#    sed -i "s|in_dataset_index: ./data/tmp_shadow/in.*|in_dataset_index: ./data/tmp_shadow/in_${i}.npy|" $yaml_file
+for i in $(seq 1 $number_shadow_model); do
+    sed -i "s|in_model_save_path: ./data/models_shadow/.*|in_model_save_path: ./data/models_shadow/model_in_${i}.pth|" $yaml_file
+    sed -i "s|out_dataset: ./data/tmp_shadow/cifar_out.*|out_dataset: ./data/tmp_shadow/cifar_out_${i}.beton|" $yaml_file
+    sed -i "s|in_dataset: ./data/tmp_shadow/cifar_in.*|in_dataset: ./data/tmp_shadow/cifar_in_${i}.beton|" $yaml_file
+    sed -i "s|out_dataset_index: ./data/tmp_shadow/out.*|out_dataset_index: ./data/tmp_shadow/out_${i}.npy|" $yaml_file
+    sed -i "s|in_dataset_index: ./data/tmp_shadow/in.*|in_dataset_index: ./data/tmp_shadow/in_${i}.npy|" $yaml_file
 
-    #python write_datasets_shadow.py --config-file $yaml_file
+    python write_datasets_shadow.py --config-file $yaml_file
 
-#    python ./scripts/generate_obs.py --config-file $yaml_file
+    python ./scripts/generate_obs.py --config-file $yaml_file
 
-#done
+done
 
 python ./scripts/statics.py
 
